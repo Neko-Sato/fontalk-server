cc070bea0753352c560119280e40c86a7175b006
Hisatoshi Shimizu <47973835+Neko-Sato@users.noreply.github.com>
2021-03-27 02:00:08 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,1 +1,9 @@
+from database import User
 
+class teat:
+  def __init__(self):
+      pass 
+  def test(self, user_id):
+      return {\
+        "message": "your userid is {}".format(user_id)
+      }
