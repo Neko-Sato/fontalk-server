9e68ccb6081e2199e7f08d743a8fefdc07682f06
Neko-Sato <hisatosi35350@gmail.com>
2021-04-01 18:07:50 +0900

    main

--- fontalk/talk/models.py
+++ fontalk/talk/models.py
@@ -1,1 +1,19 @@
 from . import db
+
+#class Talk(db.Model):
+#  id = db.Column(db.Integer, primary_key=True)
+#  name = db.Column(db.VARCHAR(20))
+#  def __repr__(self):
+#    return '<talk {}>'.format(self.id)
+
+#class Member(db.Model):
+#  id = db.Column(db.Integer, primary_key=True)
+#  talk = db.relationship('talk')
+#  user = db.relationship('user')
+#  def __repr__(self):
+#    return '<member {}>'.format(self.id)
+
+#class Message(db.Model):
+#  id = db.Column(db.Integer, primary_key=True)
+#  def __repr__(self):
+#    return '<message {}>'.format(self.id)
